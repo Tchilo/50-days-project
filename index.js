bd35632ecb1a5b70a467553d0ebf83aa78705d2f
Tchilo <tchiloross@gmail.com>
2021-11-30 16:13:23 +0200

    Complete progress function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const next = document.getElementById('next');
 const prev = document.getElementById('prev');
 const progress = document.getElementById('progress');
-const nums = document.querySelectorAll('num');
+const nums = document.querySelectorAll('.num');
 
 let currentNum = 1;
 const update = () => {
@@ -11,23 +11,37 @@ const update = () => {
     } else {
       num.classList.remove('active');
     }
-    num.classList.add('active');
   });
+  const actives = document.querySelectorAll('.active');
+  progress.style.width = `${((actives.length - 1) / (nums.length - 1)) * 100}%`;
+
+  if (currentNum === 1) {
+    prev.disabled = true;
+  } else if (currentNum === nums.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 };
 
-// Next function
+// Next btn
 
 next.addEventListener('click', () => {
+  update();
   currentNum++;
   if (currentNum > nums.length) {
     currentNum = nums.length;
   }
+  update();
 });
 
+// prev btn
 prev.addEventListener('click', () => {
   currentNum--;
 
   if (currentNum < 1) {
     currentNum = 1;
   }
+  update();
 });
