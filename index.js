470bcff15cb5ed8f424484df4bbbe9113b94252a
Tchilo <tchiloross@gmail.com>
2021-12-01 22:56:52 +0200

    add styling

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const bars = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const ul = document.querySelector('.ul');
 
 bars.addEventListener('click', () => {
     container.classList.add('show-nav');
+    ul.classList.remove('d-none');
+
 });
