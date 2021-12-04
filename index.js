aa34a409910c1fa819bdadaad33e5f0bf33c6b91
Tchilo <tchiloross@gmail.com>
2021-12-04 22:01:12 +0200

    Add Js functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+const boxes = document.querySelectorAll('.content');
+
+const checkBoxes = () => {
+  const trigger = window.innerHeight / 5 * 4;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < trigger) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
+};
+window.addEventListener('scroll', checkBoxes);
