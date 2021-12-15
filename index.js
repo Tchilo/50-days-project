8c6d918d3aaf58327620d24f04fd99bd3b54da50
Tchilo <tchiloross@gmail.com>
2021-12-15 23:15:23 +0200

    Add html, css and js

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
-const cups = document.querySelectorAll('.cup-small');
-const liters = document.getElementById('liters');
-const remained = document.getElementById('remained');
-const percentage = document.getElementById('percentage');
 
-const updateBigCup = () => {
-  const fullCups = document.querySelectorAll('.cup-small.full').length;
-  const totalCups = cups.length;
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
-  if (fullCups === 0) {
-    percentage.style.visibility = 'hidden';
-    percentage.style.height = 0;
-  } else {
-    percentage.style.visibility = 'visible';
-    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
-    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
-  }
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
-  if (fullCups === totalCups) {
-    remained.style.visibility = 'hidden';
-    remained.style.height = 0;
-  } else {
-    remained.style.visibility = 'visible';
-    liters.innerText = `${2 - ((250 * fullCups) / 1000)}L `;
-  }
-};
-updateBigCup();
+// Get initial movies
+getMovies(API_URL)
 
-const highlightCups = (index) => {
-  if (cups[index].classList.contains('full') && !cups[index].nextElementSibling.classList.contains('full')) {
-    index -= 1;
-  }
-  cups.forEach((cup, index2) => {
-    if (index2 <= index) {
-      cup.classList.add('full');
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
     } else {
-      cup.classList.remove('full');
+        return 'red'
     }
-  });
-  updateBigCup();
-};
+}
 
-cups.forEach((cup, index) => {
-  cup.addEventListener('click', () => highlightCups(index));
-});
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-localStorage.setItem('water' )
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
